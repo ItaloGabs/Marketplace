afb15bbbf6c7207187075e828d65c8a04cb5ded0
Ítalo Gabriel <gabrielgg.tavares@gmail.com>
2023-08-10 21:23:18 -0300

    Inicio Projeto

--- Themes/MarketPlace/footer.php
+++ Themes/MarketPlace/footer.php
@@ -1,9 +1,12 @@
  <?php wp_footer(); ?>
- <div class="credits">
-            <div class="responsive-container">
-            <h3>Illustrations by Freepik</h3>
-        <a href='https://www.freepik.com/' target="_blank">Check out their amazing work - www.freepik.com</a>
-            </div>
+    <footer>
+        <p>&copy; Eviltsuma.Todos Os Direitos Reservados</p>
+        <div class="social-media">
+        <p><i class="fab fa-facebook-f"></i></p>
+        <p><i class="fab fa-twitter"></i></p>
+        <p><i class="fab fa-instagram"></i></p>
+        <p><i class="fab fa-linkedin-in"></i></p>
         </div>
+    </footer>
     </body>
 </html>
---
+++ Themes/MarketPlace/scripts/filter.js
@@ -0,0 +1,19 @@
+// filter search
+$(function(){ 
+
+    $("#filtro").keyup(function(){
+      var texto = $(this).val();
+      
+      $(".card-news").each(function(){
+        var resultado = $(this).text().toUpperCase().indexOf(' '+texto.toUpperCase());
+        
+        if(resultado < 0) {
+          $(this).fadeOut();
+        }else {
+          $(this).fadeIn();
+        }
+      }); 
+  
+    });
+  
+  });
