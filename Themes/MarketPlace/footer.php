4e71b74fc04a8b255524a06c7735f880ffb152ea
Arthur Clemente <arthurclementte@hotmail.com>
2023-09-25 11:22:11 -0300

    Adicionado o estilo que centralizao o footer.

--- Themes/MarketPlace/footer.php
+++ Themes/MarketPlace/footer.php
@@ -1,12 +1,14 @@
  <?php wp_footer(); ?>
     <footer class="footer-area">
-          <p>&copy; Eviltsuma.Todos Os Direitos Reservados</p>
-          <div class="social-media">
-          <p><i class="fab fa-facebook-f"></i></p>
-          <p><i class="fab fa-twitter"></i></p>
-          <p><i class="fab fa-instagram"></i></p>
-          <p><i class="fab fa-linkedin-in"></i></p>
-          </div>
+      <div class="footer-content default-centralize">
+        <p>&copy; Eviltsuma.Todos Os Direitos Reservados</p>
+        <div class="social-media">
+        <p><i class="fab fa-facebook-f"></i></p>
+        <p><i class="fab fa-twitter"></i></p>
+        <p><i class="fab fa-instagram"></i></p>
+        <p><i class="fab fa-linkedin-in"></i></p>
+        </div>
+      </div>
     </footer>
     </body>
 </html>
